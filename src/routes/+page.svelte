933c1a9636bd0d8013dcc1897fcd0c2b93de2ccf
imranyy <imranmatano6@gmail.com>
2023-01-30 20:23:43 +0300

    dynamic routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,23 @@
 <script>
     /** @type {import('./$types').PageData} */
      export let data;
+    let title=`For you`;
 
-    let title=`For you`
+    setTimeout(()=>{
+        // @ts-ignore
+        document.querySelector('.tag').style.display='none'
+    },3000)
 </script>
 
 <div class="index start">
+   <header>
     <h1 class="title">{title}</h1>
     <p class="sub-title">Recommended based your interests</p>
+   </header>
     <ul>
         {#if data.data}
             {#each data.data as item}
-                <li>{item.name}</li>
+                <li><a href={`/blogs/${item.id}`}>{item.name}</a></li>
             {/each}
         {:else}
         <div class="tag">
@@ -20,24 +26,30 @@
         {/if}
     </ul>
     <div class="blog-tag">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-    </div>
-    <div class="blog-tag">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-    </div>
-    <div class="blog-tag">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-    </div>
-    <div class="blog-tag">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-    </div>
-    <div class="blog-tag">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-    </div>
-    <div class="blog-tag">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-    </div>
-    <div class="blog-tag">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
+        <a href='/' class="blog-item">
+           <div class="blog-title">
+                <!-- svelte-ignore a11y-img-redundant-alt -->
+                <a href="/img1.jpg"><img src='/img1.jpg' alt="blog image"/></a>
+                <p>Lorem ipsum dolor sit amet consectetu</p>
+           </div>
+            <p class="snippet">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
+        </a>
+        <a href='/' class="blog-item">
+           <div class="blog-title">
+                <!-- svelte-ignore a11y-img-redundant-alt -->
+                <a href="/img2.jpg"><img src='/img2.jpg' alt="blog image"/></a>
+                <p>Lorem ipsum dolor sit amet consectetu</p>
+           </div>
+            <p class="snippet">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
+        </a>
+        <a href='/' class="blog-item">
+           <div class="blog-title">
+                <!-- svelte-ignore a11y-img-redundant-alt -->
+                <a href="/img3.jpg"><img src='/img3.jpg' alt="blog image"/></a>
+                <p>Lorem ipsum dolor sit amet consectetu</p>
+           </div>
+            <p class="snippet">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
+        </a>
+        
     </div>
 </div>
--- src/routes/blogs/[slug]/+page.svelte
+++ src/routes/blogs/[slug]/+page.svelte
@@ -2,8 +2,8 @@
 
 </script>
 
-<div class="about">
-    <h1>About Us</h1>
+<div class="about start">
+    <h1>Detail page</h1>
     <a href="/">Home</a>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti obcaecati at quo a fugiat pariatur, vitae commodi neque nostrum quibusdam?</p>
 </div>
