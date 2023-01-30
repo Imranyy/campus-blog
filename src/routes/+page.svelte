deafe592e1df057269a6cf39e184893728dd982f
imranyy <imranmatano6@gmail.com>
2023-01-30 11:38:41 +0300

    script tag and style tag in svelte components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let title=`Welcome to campus blog`
+</script>
+
+<h1>{title}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
+
+<style>
+    h1{
+        color:red;
+    }
+</style>
