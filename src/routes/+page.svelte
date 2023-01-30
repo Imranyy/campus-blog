0fecfee54e1dd6a322ce1c1a11ce96b468a9ee5e
imranyy <imranmatano6@gmail.com>
2023-01-30 11:47:16 +0300

    bind:value in an input --in svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@
 <h1>{title}</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
 <button on:click={updateTitle}>Update Title</button>
+<input type="text" bind:value={title} name="" id=""/>
 <style>
     h1{
         color:red;
