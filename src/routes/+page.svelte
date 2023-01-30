b2287d07209a3bf171a9509c8aa40af9f4d2e9b3
imranyy <imranmatano6@gmail.com>
2023-01-30 13:56:57 +0300

    passing props to components in svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,23 @@
 <script>
+    import Nav from '$lib/nav.svelte';
+
     let title=`Welcome to campus blog`
     const updateTitle=()=>{
         title=`Hello campus people`
     }
 </script>
 
-<h1>{title}</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-<button on:click={updateTitle}>Update Title</button>
-<input type="text" bind:value={title} name="" id=""/>
-<a href="/about">about</a>
+<Nav/>
+<div class="index">
+    <h1>{title}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
+    <button on:click={updateTitle}>Update Title</button>
+    <input type="text" bind:value={title} name="" id=""/>
+    <a href="/about">about</a>
+</div>
+
 <style>
+   
     h1{
         color:red;
     }
