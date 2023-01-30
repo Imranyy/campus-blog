0527dbf2343d7f19867cd4ad929738206efac359
imranyy <imranmatano6@gmail.com>
2023-01-30 18:33:55 +0300

    started developing the campus blog after learning svelte

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -4,6 +4,15 @@
 
 <nav>
     <div class="nav-bar">
-        <h2>{brand}</h2>
+        <div class="nav-top">
+            <div class="logo"><h2>{brand}</h2></div>
+            <input type="search" name="" placeholder="&#128269;  News Feed" id=""/>
+        </div>
+        <div class="nav-links">
+            <ul role="navigation">
+                <li><a href="/home">Home</a></li>
+                <li><a href="/">For you</a></li>
+            </ul>
+        </div>
     </div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,10 @@
     /** @type {import('./$types').PageData} */
      export let data;
 
-    let title=`Welcome to campus blog`
-    const updateTitle=()=>{
-        title=`Hello campus people`
-    }
+    let title=`For you`
 </script>
 
-<div class="index">
+<div class="index start">
     <h1 class="title">{title}</h1>
     <h3>list</h3>
     <ul>
@@ -21,7 +18,5 @@
         {/if}
     </ul>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-    <button on:click={updateTitle}>Update Title</button>
-    <input type="text" bind:value={title} name="" id=""/>
     <a href="/about">about <i class="fa fa-user"></i></a>
 </div>
