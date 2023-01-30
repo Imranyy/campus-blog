328ae75e4a523ba4ff7d083033777cc4b24baa1d
imranyy <imranmatano6@gmail.com>
2023-01-30 14:49:00 +0300

    global css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,16 +7,9 @@
 </script>
 
 <div class="index">
-    <h1>{title}</h1>
+    <h1 class="title">{title}</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
     <button on:click={updateTitle}>Update Title</button>
     <input type="text" bind:value={title} name="" id=""/>
     <a href="/about">about <i class="fa fa-poo"></i></a>
 </div>
-
-<style>
-  
-    h1{
-        color:red;
-    }
-</style>
