472fbe67ce7c5a943dc67a2b810ca62253df58b1
imranyy <imranmatano6@gmail.com>
2023-01-31 09:54:56 +0300

    creating blogs backend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,42 +14,22 @@
     <h1 class="title">{title}</h1>
     <p class="sub-title">Recommended based your interests</p>
    </header>
-    <!-- <ul>
+    <div class="blog-tag">
         {#if data.data}
             {#each data.data as item}
-                <li><a href={`/blogs/${item.id}`}>{item.name}</a></li>
+            <a href={`/blogs/${item.id}`} class="blog-item">
+                <div class="blog-title">
+                     <!-- svelte-ignore a11y-img-redundant-alt -->
+                     <a href={item.image}><img src={item.image} alt="blog image"/></a>
+                     <p>{item.title}</p>
+                </div>
+                 <p class="snippet">{item.snippet}</p>
+             </a>
             {/each}
         {:else}
-        <div class="tag">
-            <p class="error">{data.error}</p>
-        </div>
+            <div class="tag">
+                <p class="error">{data.error}</p>
+            </div>
         {/if}
-    </ul> -->
-    <div class="blog-tag">
-        <a href='/' class="blog-item">
-           <div class="blog-title">
-                <!-- svelte-ignore a11y-img-redundant-alt -->
-                <a href="/img1.jpg"><img src='/img1.jpg' alt="blog image"/></a>
-                <p>Lorem ipsum dolor sit amet consectetu</p>
-           </div>
-            <p class="snippet">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-        </a>
-        <a href='/' class="blog-item">
-           <div class="blog-title">
-                <!-- svelte-ignore a11y-img-redundant-alt -->
-                <a href="/img2.jpg"><img src='/img2.jpg' alt="blog image"/></a>
-                <p>Lorem ipsum dolor sit amet consectetu</p>
-           </div>
-            <p class="snippet">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-        </a>
-        <a href='/' class="blog-item">
-           <div class="blog-title">
-                <!-- svelte-ignore a11y-img-redundant-alt -->
-                <a href="/img3.jpg"><img src='/img3.jpg' alt="blog image"/></a>
-                <p>Lorem ipsum dolor sit amet consectetu</p>
-           </div>
-            <p class="snippet">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, fuga quos. Placeat magnam dolor officia consequatur dicta accusamus ab reiciendis iste, impedit sunt, nobis, repudiandae sed aut voluptatem delectus blanditiis.</p>
-        </a>
-        
     </div>
 </div>
